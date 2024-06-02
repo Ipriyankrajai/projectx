9fca3af811d308a2ead4e027a7b3fa826a51ad50
Priyank Rajai <priyank73@hotmail.com>
2024-06-02 15:33:11 +0530

    Feat: header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+function Header() {
+  return (
+    <header className="px-5 py-[10px]">
+      <nav>
+        <Link href={"/"} className="font-semibold text-[20px]">
+          StealThe<span className="text-red-600">Deal</span>
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
