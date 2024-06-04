5bf2682c0905cb9501de747db96370f393753a5c
Priyank Rajai <priyank73@hotmail.com>
2024-06-04 22:32:51 +0530

    Feat: maxWidthWrapper

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div
+      className={cn("h-full mx-auto w-full max-w-screen-xl px-5", className)}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
